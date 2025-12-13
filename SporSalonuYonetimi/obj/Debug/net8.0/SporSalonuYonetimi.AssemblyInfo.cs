791b4144408d1ba6f09ff1801579ddcfca9f918e
elifsenasoysal <168901373+elifsenasoysal@users.noreply.github.com>
2025-12-13 22:45:16 +0300

    CRUD

--- SporSalonuYonetimi/obj/Debug/net8.0/SporSalonuYonetimi.AssemblyInfo.cs
+++ SporSalonuYonetimi/obj/Debug/net8.0/SporSalonuYonetimi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SporSalonuYonetimi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbf64c5f655cac51148a5ea5b7b6feee896462ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d0ba9b25a6fbb54ec28ef945930ca737c27dabf")]
 [assembly: System.Reflection.AssemblyProductAttribute("SporSalonuYonetimi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SporSalonuYonetimi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
